250f02e0d810feff0b068f77edab2f5719e0e5a0
Rusel T. Tayong <ruseltayong@gmail.com>
2024-01-23 22:53:41 +0800

    PushUPdates

--- app/Http/Controllers/ServicesController.php
+++ app/Http/Controllers/ServicesController.php
@@ -7,6 +7,10 @@ use App\Models\Services;
 
 class ServicesController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
     public function services($category) {
         return services::where("category","like","%".$category."%")->get();
     }
